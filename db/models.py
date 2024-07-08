787308187a94e23983d8505e05c17b51e5d70881
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-07-08 19:40:47 +0300

    fix: custom view

--- db/models.py
+++ db/models.py
@@ -1,9 +1,13 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Enum
 from sqlalchemy.orm import relationship
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.templating import Jinja2Templates
 from starlette_admin import CustomView
 from starlette_admin.contrib.sqla import Admin, ModelView
 
-from db.engine import Base, engine
+import crud
+from db.engine import Base, engine, SessionLocal
 from db.transaction_type import TransactionType
 
 
@@ -27,12 +31,30 @@ class DBTransaction(Base):
     user = relationship(DBUser)
 
 
+class StatisticAdminView(CustomView):
+    def total_transactions(self):
+        count = crud.total_transactions(db=SessionLocal())
+        amount = crud.total_amount(db=SessionLocal())
+        return {"count": count, "amount": amount}
+
+    async def render(
+        self, request: Request, templates: Jinja2Templates
+    ) -> Response:
+        """Default methods to render view. Override this methods to add your custom logic."""
+        context = self.total_transactions()
+        context.update({"request": request, "title": self.title(request)})
+
+        return templates.TemplateResponse(self.template_path, context)
+
+
 # Create an empty admin interface
 admin = Admin(engine, title="User-Transaction")
 
 admin.add_view(
-    CustomView(
-        label="Statistic", template_path="statistic.html", add_to_menu=False
+    StatisticAdminView(
+        label="Statistic",
+        template_path="statistic.html",
+        add_to_menu=False,
     )
 )
 admin.add_view(ModelView(DBUser))
